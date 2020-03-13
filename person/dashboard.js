cde4dfea2063fc89c83eaae7f93267a917d0b66d
cagurl01 <cagurl01@louisville.edu>
2020-03-13 10:57:35 -0400

    Further updates to document structure, removed old markup

--- person/dashboard.js
+++ person/dashboard.js
@@ -1,7 +1,7 @@
 'use strict';
-$('.dashcontent').each(function () {
-  if (!$(this).html()) {
+$('.dash-content').each(function () {
+  if (!$(this).text()) {
     $(this).html('<em>none</em>');
   }
 });
-$('.dashheader').click(function () { $(this).next().toggle(); });
+$('.dash-header').click(function () { $(this).next().toggle(); });
