cc18978f2413ad8de7153dc46a1140b8306b855d
cagurl01 <cagurl01@louisville.edu>
2020-03-16 17:32:01 -0400

    Implemented flexbox

--- person/dashboard.js
+++ person/dashboard.js
@@ -12,3 +12,4 @@ $('.dash-header').on('mousedown mouseup', function () {
   $(this).parent().toggleClass('dash-tl');
 });
 $('#dash-recdet .dash-body').hide();
+// if (window.matchMedia('(max-width: 575px)')) {}
